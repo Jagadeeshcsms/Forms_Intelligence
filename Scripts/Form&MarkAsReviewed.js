61fc165cae511a41fdf2315cf0a86e948efbc69b
jagadeesh.chandrasekar <jagadeesh.chandrasekar@mrisoftware.com>
2023-06-05 13:46:48 +0000

    Appvance Commit [skip ci] - [adding Scripts/Form&MarkAsReviewed.js]

--- Scripts/Form&MarkAsReviewed.js
+++ Scripts/Form&MarkAsReviewed.js
@@ -1,8 +1,8 @@
-var pageno = parseInt(getText(byXPath('(//*[@id="pagePredict"]/div[1]/p/span[3]//button)[last()]')));
+var pageno = parseInt(getText(byXPath('(//*[@id="rootdiv"]/div/div/div[2]/div/div/div[2]/div[2]/span[4]//button)[last()]')));
 var pageno = parseInt(getText(byXPath('(//*[//*[@id="rootdiv"]/div/div/div[2]/div/div/div[2]/div[3]/button[1]/span//button)[last()]')));
 if (i < pageno) {
         var ind = i + 1;
-        click(byXPath('(//*[@id="pagePredict"]/div[1]/p/span[3]//button)[' + ind + ']'));
+        click(byXPath('(//*[//*[@id="rootdiv"]/div/div/div[2]/div/div/div[2]/div[2]/span[4]//button)[' + ind + ']'));
         click(byXPath('(//*[@id="rootdiv"]/div/div/div[2]/div/div/div[2]/div[3]/button[1]//button)[' + ind + ']'));
         wait(3000);
 }
