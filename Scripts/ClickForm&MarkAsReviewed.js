6949e26b6c069dc44ae83850564bc232f0276ef9
jagadeesh.chandrasekar <jagadeesh.chandrasekar@mrisoftware.com>
2023-06-06 13:30:24 +0000

    Appvance Commit [skip ci] - [adding Scripts/ClickForm&MarkAsReviewed.js]

--- Scripts/ClickForm&MarkAsReviewed.js
+++ Scripts/ClickForm&MarkAsReviewed.js
@@ -2,6 +2,7 @@ var arr = getText(span("review-form-change-count")).split('/');
 var docno = parseInt(arr[1]);
 log(docno);
 for (i = 1; i <= docno; i++) {
+  	wait(500)
     click(span("review-form-change-next"));
     wait(1000)
     click(button("Mark as reviewed"));
