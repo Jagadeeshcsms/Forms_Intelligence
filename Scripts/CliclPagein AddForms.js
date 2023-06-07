c2a5bc53019e248bd3f88cf33050a0ea18eda4be
jagadeesh.chandrasekar <jagadeesh.chandrasekar@mrisoftware.com>
2023-06-07 07:13:58 +0000

    Appvance Commit [skip ci] - [adding Scripts/CliclPagein AddForms.js]

---
+++ Scripts/CliclPagein AddForms.js
@@ -0,0 +1,14 @@
+var pageno = parseInt(getText(byXPath('(//div[@class="k-pager-numbers"]//button)[last()]')));
+log(pageno);
+for (i = 1; i <= pageno; i++) {
+    var ChkNo = parseInt(_eval("ds$('table tbody tr').find('td:contains(\"Uploaded\")').length"));
+    log(ChkNo);
+    for (k = 1; k <= ChkNo; k++) {
+        click(byXPath('(//td[contains(text(),"Uploaded")]//parent::tr//input[@type="checkbox"])[' + k + ']'));
+    }
+    if (i < pageno) {
+        var ind = i + 1;
+        click(byXPath('(//div[@class="k-pager-numbers"]//button)[' + ind + ']'));
+        wait(3000);
+    }
+}
