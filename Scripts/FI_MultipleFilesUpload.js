a4f8a55e99b3e617987d867043a25a7da3926458
jagadeesh.chandrasekar <jagadeesh.chandrasekar@mrisoftware.com>
2023-06-08 21:39:07 +0000

    DS3 - [adding Scripts/FI_MultipleFilesUpload.js]

--- Scripts/FI_MultipleFilesUpload.js
+++ Scripts/FI_MultipleFilesUpload.js
@@ -2,7 +2,7 @@ for(i=1;i<=2;i++){
 click(span(any("Add Forms","mri-button__label")));
 wait(30000);
 setBulkFiles2(byXPath('//*[@type="file"]'),"{ds}/../TestData/10files/");
-wait(30000);
+wait(50000);
 click(span(0, _in(button("Upload"))));
 wait(20000);
 }
