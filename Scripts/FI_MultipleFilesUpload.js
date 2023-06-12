c046e4225a93d75a9c93473838d551e3e487064d
jagadeesh.chandrasekar <jagadeesh.chandrasekar@mrisoftware.com>
2023-06-12 07:26:56 +0000

    Appvance Commit [skip ci] - [adding Scripts/FI_MultipleFilesUpload.js]

--- Scripts/FI_MultipleFilesUpload.js
+++ Scripts/FI_MultipleFilesUpload.js
@@ -1,8 +1,8 @@
 for(i=1;i<=2;i++){
 click(span(any("Add Forms","mri-button__label")));
-wait(30000);
+wait(40000);
 setBulkFiles2(byXPath('//*[@type="file"]'),"{ds}/../TestData/10files/");
-wait(30000);
+wait(40000);
 click(span(0, _in(button("Upload"))));
 wait(20000);
 }
