3fdf6eac13e008468cab47138a77aa5fbfdf7f14
jagadeesh.chandrasekar <jagadeesh.chandrasekar@mrisoftware.com>
2023-06-12 08:03:29 +0000

    Appvance Commit [skip ci] - [adding Scripts/BeforeExtractSelctAllDocuments.js]

--- Scripts/BeforeExtractSelctAllDocuments.js
+++ Scripts/BeforeExtractSelctAllDocuments.js
@@ -3,10 +3,10 @@ log(pageno);
 for (i = 1; i <= pageno; i++) {
     var ChkNo = parseInt(_eval("ds$('table tbody tr').find('td:contains(\"Uploaded\")').length"));
     log(ChkNo);
-  	wait(2000);
+  	wait(20000);
     for (k = 1; k <= ChkNo; k++) {
         click(byXPath('(//td[contains(text(),"Uploaded")]//parent::tr//input[@type="checkbox"])[' + k + ']'));
-      wait(2000);
+      wait(20000);
     }
     if (i < pageno) {
         var ind = i + 1;
