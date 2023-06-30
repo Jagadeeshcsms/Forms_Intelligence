35b330aeaa2b4024b81d579f3e07f7d5abe3f882
naveen8827 <naveen8827>
2023-06-30 11:17:05 +0000

    Appvance Commit [skip ci] - [adding Scripts/FI_MultipleFilesUpload.js]

--- Scripts/FI_MultipleFilesUpload.js
+++ Scripts/FI_MultipleFilesUpload.js
@@ -3,6 +3,6 @@ click(span(any("Add Forms","mri-button__label")));
 wait(10000,isVisible(div(["upload-header"])));
 setBulkFiles2(byXPath('//*[@type="file"]'),"{ds}/../TestData/10files/");
 wait(200000,isVisible(button(["Upload"])));
-click(button(["Upload"]));
+click(span("Upload"));
 wait(10000,isVisible(span(["File Upload in Progress"])));
 }
